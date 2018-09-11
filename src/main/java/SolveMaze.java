cce434fc56ca781a98da1eb0aba6b4f39f9be9c4
jakobsa2 <jakobsa2@illinois.edu>
2018-09-11 18:11:53 -0500

    Made right-hand algorithm

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -33,8 +33,34 @@ public class SolveMaze {
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
+        int count = 0;
+        int runOnce = 0;
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                count += 1;
+                maze.turnRight();
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnLeft();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnLeft();
+                        if (maze.canMove()) {
+                            maze.move();
+                        } else {
+                            maze.turnLeft();
+                            maze.move();
+                        }
+                    }
+                }
+            }
+            if (runOnce == 0) {
+                System.out.println(count);
+                runOnce++;
+            }
         }
 
         if (maze.isFinished()) {
